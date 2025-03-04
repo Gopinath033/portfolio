37a1d764e0743d133789a432bc5444770455f88b
Gopinath_Munusamy <gopi.messi33@gmail.com>
2025-03-04 21:13:18 +0530

    updated about, skills, education and contact section

---
+++ index.js
@@ -0,0 +1,28 @@
+async function fetchData(type = "skills") {
+    let response;
+    type === "skills" ?
+        response = await fetch("skills.json")
+        :
+        response = await fetch("./projects.json")
+    const data = await response.json();
+    return data;
+}
+
+function showSkills(skills) {
+    let skillsContainer = document.getElementById("skillsContainer");
+    let skillHTML = "";
+    skills.forEach(skill => {
+        skillHTML += `
+        <div class="bar">
+              <div class="info">
+                <img src=${skill.icon} alt="skill" />
+                <span>${skill.name}</span>
+              </div>
+            </div>`
+    });
+    skillsContainer.innerHTML = skillHTML;
+}
+
+fetchData().then(data => {
+    showSkills(data);
+});
